0b63f3a6ed75ffb7a2a62546ce7c685f92bc6edb
jessyfpontes <137102252+jessyfpontes@users.noreply.github.com>
2024-11-01 13:23:36 -0300

    Add files via upload

---
+++ banco.php
@@ -0,0 +1,44 @@
+<?php
+ 
+class Banco
+{
+    private static $dbNome = 'db_help';
+    private static $dbHost = 'localhost';
+    private static $dbUsuario = 'root';
+    private static $cont = null;
+ 
+    public function __construct()
+    {
+        die('A função Init não é permitida!');
+    }
+ 
+    public static function conectar()
+    {
+        if (null === self::$cont) { //aaaaaa
+            try {
+                self::$cont = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbNome, self::$dbUsuario);
+                self::$cont->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $exception) {
+                die('Erro de conexão: ' . $exception->getMessage());
+            }
+        }
+        return self::$cont;
+    }
+ 
+    public static function desconectar()
+    {
+        self::$cont = null;
+    }
+}
+ 
+// Exemplo de uso
+try {
+    $conexao = Banco::conectar();
+    // Aqui você pode realizar suas operações no banco de dados
+} catch (Exception $e) {
+    echo 'Erro: ' . $e->getMessage();
+} finally {
+    Banco::desconectar();
+}
+ 
+?>
